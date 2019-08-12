186c4251bfd82a48ce806c76b3cd5d15178310d5
Jingyuan Hu <hujingyuan@icarbonx.com>
2019-08-12 15:37:21 +0800

    update

---
+++ src/mock/todayData.tsx
@@ -0,0 +1,32 @@
+interface Item {
+  type: string;
+  count: number;
+}
+const todayData: Item[] = [
+  {
+    type: '工作',
+    count: 15,
+  },
+  {
+    type: '学习',
+    count: 23,
+  },
+  {
+    type: '思考',
+    count: 5,
+  },
+  {
+    type: '写作',
+    count: 3,
+  },
+  {
+    type: '运动',
+    count: 2,
+  },
+  {
+    type: '阅读',
+    count: 10,
+  },
+];
+
+export default todayData;
--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import * as echarts from 'echarts';
+import style from './index.module.scss';
+import todayData from '../../mock/todayData';
+
+const list: number[] = todayData.map((item) => {
+  return item.count;
+});
+let total: number = list.reduce((x, y) => x + y);
 
 const Statistics = () => {
+  // let todayChart = echarts.init(document.getElementById('todayMountNode'));
   return (
-    <div>统计页面</div>
+    <div className={style.statistics}>
+      <div className={style.items}>
+        <span className={style.item}>今日番茄数</span>
+        <span className={style.item}>累计番茄数</span>
+      </div>
+      <div className={style.todayFinish}>
+        <div className={style.title}>
+          <p>今日完成</p>
+          <p>{total}个番茄</p>
+        </div>
+        <div id="todayMountNode" className={style.todayMountNode}></div>
+      </div>
+    </div>
   );
 };
 
